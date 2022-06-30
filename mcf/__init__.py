de6ec6c87c7b1e7ed872302e0054b691e4081e39
Dev Goldy <jassim7256@gmail.com>
2022-07-01 00:50:40 +0100

    changed mcf commands to sub commands
    extension will error if not on latest dev version of Goldy Bot! (v4.0dev11 or above)

--- mcf/__init__.py
+++ mcf/__init__.py
@@ -7,18 +7,19 @@ from . import _forms_, _info_, _tournament_, _player_
 from GoldyBot.utility.datetime.user_input import *
 from GoldyBot.utility.commands import *
 
-from errors import TournamentDoesntExistError
-
 class MCF(GoldyBot.Extenstion):
     def __init__(self, package_module=None):
         super().__init__(self, package_module_name=package_module)
 
     def loader(self):
+        @GoldyBot.command(slash_cmd_only=True)
+        async def mcf(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
+            pass
 
         # mcf join command
         #======================
-        @GoldyBot.command(help_des="An amazing command to sign up for the mcf minecraft tournament right from the confort of Discord.", slash_cmd_only=True)
-        async def join_mcf(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
+        @mcf.sub_command(help_des="An amazing command to sign up for the mcf minecraft tournament right from the confort of Discord.")
+        async def join(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             # Check if a joinable mcf tournament exists.
             tournament = await _info_.TournamentInfo().get_latest_mcf()
 
@@ -39,21 +40,21 @@ class MCF(GoldyBot.Extenstion):
 
                 await message.delete()
 
-        @GoldyBot.command(help_des="Admin command to open the mcf minecraft tournament forum.", slash_cmd_only=True, required_roles=["nova_staff"])
-        async def mcf_open_form(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
+        @mcf.sub_command(help_des="Admin command to open the mcf minecraft tournament forum.", required_roles=["nova_staff"])
+        async def open_form(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             await ctx.send_modal(_forms_.OpenMCFForm())
 
-        @GoldyBot.command(help_des="Admin command for canceling an MCF tournament.", slash_cmd_only=True, required_roles=["nova_staff"],
+        @mcf.sub_command(help_des="Admin command for canceling an MCF tournament.", required_roles=["nova_staff"],
         slash_options={
             "mcf_date":nextcord.SlashOption("mcf_date", "Date of the mcf your trying to cancel."),
             "mcf_time":nextcord.SlashOption("mcf_time", "Time of the mcf your trying to cancel.")
         })
-        async def mcf_cancel(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
+        async def cancel(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             tournament_info = _info_.TournamentInfo()
             
             view = _forms_.MCFCancelDropdownView(GoldyBot.Member(ctx), await tournament_info.get_all_mcfs())
 
-            await ctx.send(embed=GoldyBot.utility.goldy.embed.Embed(
+            await send(ctx, embed=GoldyBot.utility.goldy.embed.Embed(
                 title="🛑 Which MCF?",
                 description="**⏱ Pick the mcf you would like to cancel.**",
                 colour=GoldyBot.utility.goldy.colours.WHITE
