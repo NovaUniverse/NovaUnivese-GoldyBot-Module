b835674625501bde19c37c2aedaa2cfd83c8b8af
Dev Goldy <jassim7256@gmail.com>
2022-07-29 00:52:58 +0100

    started working on it again :)

--- mcf/__init__.py
+++ mcf/__init__.py
@@ -11,6 +11,8 @@ class MCF(GoldyBot.Extenstion):
     def __init__(self, package_module=None):
         super().__init__(self, package_module_name=package_module)
 
+        self.tournament:_tournament_.MCFTournament = None
+
         # Embeds
         #==========
         self.no_mcf_embed = GoldyBot.utility.goldy.embed.Embed(
@@ -40,53 +42,48 @@ class MCF(GoldyBot.Extenstion):
     def loader(self):
         @GoldyBot.command(slash_cmd_only=True)
         async def mcf(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
-            pass
+            self.tournament = await _info_.TournamentInfo().get_latest_mcf()
+
+            if not self.tournament == None:
+                return True # Your good to continue to the sub command.
+
+            else:
+                # No mcf available.
+                message = await send(ctx, embed=self.no_mcf_embed)
+
+                await asyncio.sleep(6)
+
+                await message.delete()
+
+                # Stop right here! Don't continue!
+                return False
+
 
 
         @mcf.sub_command(help_des="An amazing command to sign up for the mcf minecraft tournament right from the confort of Discord.")
         async def join(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             # Check if a joinable mcf tournament exists.
-            tournament = await _info_.TournamentInfo().get_latest_mcf()
 
-            if not tournament == None:
-                # Send member form.
-                await ctx.send_modal(_forms_.JoinMCFForm(tournament))
-            else:
-                # No mcf available.
-                message = await send(ctx, embed=self.no_mcf_embed)
+            await ctx.send_modal(_forms_.JoinMCFForm(self.tournament))
 
-                await asyncio.sleep(6)
-
-                await message.delete()
 
         
         @mcf.sub_command(help_des="A command for players to leave this week's mcf tournament.")
         async def leave(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             # Check if a joinable mcf tournament exists.
-            tournament = await _info_.TournamentInfo().get_latest_mcf()
             member = GoldyBot.Member(ctx)
             
-            if not tournament == None:
-                # Check if player is registered.
-                if await tournament.is_member_registered(member):
-                    # If registered remove the player.
-                    await tournament.remove_player(await tournament.get_player(member))
-                    await send(ctx, embed=self.youve_been_removed_embed)
-                
-            else:
-                # No mcf available.
-                message = await send(ctx, embed=self.no_mcf_embed)
+            # Check if player is registered.
+            if await self.tournament.is_member_registered(member):
+                # If registered remove the player.
+                await self.tournament.remove_player(await self.tournament.get_player(member))
+                await send(ctx, embed=self.youve_been_removed_embed)
 
-                await asyncio.sleep(6)
-
-                await message.delete()
-
-
-        @mcf.sub_command(help_des="Admin command to open the mcf minecraft tournament forum.", required_roles=["nova_staff"])
+        @mcf.sub_command(help_des="Admin command to open the mcf minecraft tournament forum.", required_roles=["nova_staff"], also_run_parent_CMD=False)
         async def open_form(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             await ctx.send_modal(_forms_.OpenMCFForm())
 
-        @mcf.sub_command(help_des="Admin command for canceling an MCF tournament.", required_roles=["nova_staff"])
+        @mcf.sub_command(help_des="Admin command for canceling an MCF tournament.", required_roles=["nova_staff"], also_run_parent_CMD=False)
         async def cancel(self:MCF, ctx:GoldyBot.objects.slash.InteractionToCtx):
             tournament_info = _info_.TournamentInfo()
             
